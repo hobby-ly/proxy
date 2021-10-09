02f90cd853cca688ff9a63ce44dedfa5bd5dce61
liuyao <liuyao.21tb.com>
2021-10-09 14:38:58 +0800

    server runner.sh

--- server/src/main/resources/runner.sh
+++ server/src/main/resources/runner.sh
@@ -6,23 +6,42 @@ abPath=$(cd `dirname $0`;pwd)
 jarName=vpn-server.jar
 jarId=(`${jdkPath}/bin/jps | grep ${jarName}`)
 jarId=${jarId[0]}
-now=`date +%Y%m%d%H%M%S`
 cronPath=/var/spool/cron/`whoami`
-logPath=logs
+# cronPath=/etc/crontab
 logFileName=console.log
 
-if [ ! -d ${logPath} ]; then
-	mkdir -p ${logPath}
-	echo create folder ${logPath}
-fi
+# this line is required, because crontab path is not ${abPath}, so need cd
+cd ${abPath}
+
+help() {
+	echo -----method---------
+	echo start
+	echo stop
+	echo restart
+	echo auto_reboot
+	echo cron_start
+	echo cron_stop
+}
+
+vpnlog() {
+	logPath=logs
+	now=`date "+%Y-%m-%d %H:%M:%S"`
+
+	if [ ! -d ${logPath} ]; then
+		mkdir -p ${logPath}
+		echo create folder ${logPath}
+	fi
+
+	if [ -f ${logFileName} ]; then
+		# mv ${logFileName} ${logPath}/${now}.log
+		echo aa
+	fi
+	echo ${now} ${1} >> ${logFileName}
+}
 
 start() {
-	if [ -f ${logFileName} ];then
-		mv ${logFileName} ${logPath}/${now}.log
-		echo backup log to ${logPath}/${now}.log
-	fi
-	echo start ${abPath}/vpn-server.jar
-	$jdkPath/bin/java -jar ${abPath}/${jarName} -Xms512M -Xmx512M -XX:+HeapDumpOnOutOfMemoryError -Xlogg${abPath}/logs/gc-%t.log -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=10 -XX:GCLogFileSize=20M -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCCause  1 >& ${logFileName}  &
+	vpnlog "exec ${jdkPath}/bin/java ${jarName}"
+	${jdkPath}/bin/java -jar ${jarName} -Xms512M -Xmx512M -XX:+HeapDumpOnOutOfMemoryError -Xlogg${abPath}/logs/gc-%t.log -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=10 -XX:GCLogFileSize=20M -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCCause 2 >& 1 >> ${logFileName} &
 }
 
 stop() {
@@ -37,34 +56,23 @@ restart() {
 
 # reboot shell
 auto_reboot() {
-	echo ${now} auto_reboot start >> ${abPath}/${logFileName} 
-	
 	# appInfo=(`top -p ${jarId} -n 1 | grep ${jarId}`)
-	# 注意 这里在centos8中 第一个下标=1
-	
+	# tip this place in centos8, the first index is 1
+
 	memSizeKb=(`cat /proc/${jarId}/status | grep VmRSS`)
 	# number compare (=:-eq !=:-ne >:-gt >=:-ge <:-lt <=:-le)
 	if [ ${memSizeKb[1]} -gt 300000 ]; then
-		echo ${now} auto_reboot run >> ${abPath}/${logFileName}
-		restart	
-	fi
-}
-
-cron_start1() {
-	# if no rule
-	if [ ! -f ${cronPath} ];then
-		add_scheduler_rule
-	elif [ `grep -c "${optId}" ${cronPath}` -eq "0" ]; then
-		add_scheduler_rule
+		restart
 	fi
 }
 
 cron_start() {
+	# * \x2A encode
 	schContent="\x2A/10 \x2A \x2A \x2A \x2A sh ${abPath}/${0} auto_reboot # ${optId}"
 	# add rule
 	echo -e ${schContent} >> ${cronPath}
-	# console 
-	echo -e ------ add cron rules: ${schContent}	
+	# console
+	echo -e ------ add cron rules: ${schContent}
 }
 
 cron_stop() {
@@ -72,12 +80,11 @@ cron_stop() {
 	# cronLineNum=`cat ${cronPath} | grep -n "${optId}" | cut -d: -f1`
 	# delete rule by line number
 	# sed -i '${cronLineNum}d' ${cronPath}
-	# console
-	# echo delete ${cronLineNum} line in ${cronPath}
-	
+
 	# delete rule by string
 	sed -i "/${optId}/d" ${cronPath}
 	echo delete ${optId} line in ${cronPath}
 }
+
 
 ${1}
